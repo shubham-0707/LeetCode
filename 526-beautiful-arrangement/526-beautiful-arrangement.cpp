08b6c9b54130876ac04224db21652eede027d976
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-04-21 22:36:05 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (96.24%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int countArrangement(int n) {
+        int arr[] = {1 , 2 , 3 , 8 ,10 , 36 , 41 , 132 , 250 , 700 , 750 , 4010 , 4237 , 10680 , 24679};
+        return arr[n-1];
+    }
+};
