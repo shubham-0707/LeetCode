74ec482ebb1b25389b579aa1b5e6767656c62300
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-05-14 22:58:20 +0530

    Time: 1 ms (93.70%), Space: 39.6 MB (83.84%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==0){
+            return false;
+        }
+        else if(n==1){
+            return true;
+        }
+        else if(n%4!=0){
+            return false;
+        }
+        return isPowerOfFour(n/4);
+    }
+}
