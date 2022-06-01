b5d4a925bd45b6c6ca663762cd1578e0ba292db8
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-06-01 22:09:27 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (8.45%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,14 +1,8 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        int sum = 0;
-        
-        for(int i=0 ; i<nums.length ; i++){
-            sum = sum+nums[i];
-            arr.add(sum);
+        for(int i=1 ; i<nums.length ; i++){
+            nums[i]+=nums[i-1];
         }
-        
-        // to convert Integer arrayList to int array..
-        return arr.stream().mapToInt(i -> i).toArray();
+        return nums;
     }
 }
