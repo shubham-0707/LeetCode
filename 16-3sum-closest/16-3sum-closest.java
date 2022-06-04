dfd3769b58f939d29d4cee7b314fa09053e15f0d
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-06-04 22:50:52 +0530

    Time: 425 ms (6.52%), Space: 41.5 MB (98.83%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int mini = Integer.MAX_VALUE;
+        int ans = mini;
+        
+        for(int i=0 ; i<nums.length ; i++){
+            for(int j=i+1 ; j<nums.length ; j++){
+                for(int k=j+1 ; k<nums.length ; k++){
+                    int sum = Math.abs(target-(nums[i]+nums[j]+nums[k]));
+                    if(mini>sum){
+                        mini=sum;
+                        ans = nums[i]+nums[j]+nums[k];
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
