412ca793b2b254dc879582c33262bc8cca69d392
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-07-18 22:13:10 +0530

    Time: 0 ms (100.00%), Space: 43 MB (7.22%) - LeetHub

---
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int sum = 0;
+    public  void calcSum(TreeNode root , boolean isLeft){
+        if(root==null) return;
+        else if(root.left==null && root.right==null){
+            if(isLeft==true){
+                sum+=root.val;
+                return;
+            }
+        }
+        calcSum(root.left , true);
+        calcSum(root.right , false);
+        
+    }
+    
+    public int sumOfLeftLeaves(TreeNode root) {
+        calcSum(root , false);
+        return sum;
+    }
+}
