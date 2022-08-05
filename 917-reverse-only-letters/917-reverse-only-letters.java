a4d488c7805f2ffaa9657e0fa37797c19de331d7
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-06 00:31:50 +0530

    Time: 2 ms (34.23%), Space: 42.7 MB (13.71%) - LeetHub

--- 917-reverse-only-letters/917-reverse-only-letters.java
+++ 917-reverse-only-letters/917-reverse-only-letters.java
@@ -1,10 +1,12 @@
 class Solution {
     public String reverseOnlyLetters(String s) {
+        // converting the string to the charArray...
         char[] ch = s.toCharArray();
         
         int start = 0;
         int end = s.length()-1;
         
+        // Storing all the english alphabets in a hashmap so that the searching becomes easy...
         HashMap<Character , Integer> hash = new HashMap<>();
         for(int i=0 ; i<26 ;i++){
             hash.put((char)(97+i) , 1);
@@ -13,14 +15,19 @@ class Solution {
             hash.put((char)(65+i) , 1);
         }
         
+        // using two while loops ..since the constraints are too less thats why we can prefer nested loops approach..
         while(start<end){
+            
+            // interating untill start pointer reacher a good character
             while(start<end&&!hash.containsKey(ch[start])){
                 start++;
             }
+            // iterating untill the end pointer reaches the good character..
             while(end>start&&!hash.containsKey(ch[end])){
                 end--;
             }
             
+            // swapping the array elements..
             char temp = ch[start];
             ch[start] = ch[end];
             ch[end] = temp;
@@ -29,7 +36,11 @@ class Solution {
             end--;
         }
         
+        // converting the charArray to the string again..
         String ans = new String(ch);
         return ans;
+        
+        // Time Complexity : O(N) (since the loops will run only till the number of charcters in the string..)
+        // Space Complexity : O(N) since we used hashmap..
     }
 }
