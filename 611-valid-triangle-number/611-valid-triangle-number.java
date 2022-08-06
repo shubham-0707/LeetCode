3ef52e6af3bdef8635b754eae6cfa8677e4ead2e
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-06 18:24:08 +0530

    Time: 65 ms (33.93%), Space: 44.1 MB (38.87%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.java
+++ 611-valid-triangle-number/611-valid-triangle-number.java
@@ -1,15 +1,18 @@
 class Solution {
-    public int triangleNumber(int[] nums) {
+    public int triangleNumber(int[] arr) {
+        // Approach 1 : Use 3 loops but that will be O(n cube)...
+        
+        //Approach 2 : Use 2 loops and scan linearly...
+        
         int count = 0;
-        int n = nums.length;
-        Arrays.sort(nums);
-        for(int i=0 ; i<n-2 ; i++){
+        Arrays.sort(arr);
+        for(int i=0 ; i<arr.length-2 ; i++){
             int k = i+2;
-            for(int j=i+1 ; j<n-1 && nums[i]!=0 ; j++){
-                while(k<nums.length && nums[i]+nums[j]>nums[k]){
+            for(int j=i+1 ; j<arr.length-1&&arr[i]!=0 ; j++){
+                while(k<arr.length && arr[i]+arr[j]>arr[k]){
                     k++;
                 }
-                count+=k-j-1;
+                count+=(k-j-1);
             }
         }
         return count;
