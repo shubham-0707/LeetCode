ee98592f9ee957d4497b6c58d22417d7a3f7bd53
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-09 00:39:54 +0530

    Time: 6 ms (9.27%), Space: 57.9 MB (42.40%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -0,0 +1,45 @@
+class Solution {
+    public static int binary_search(int[] prefix , int key , int high){
+        int low = 0;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            
+            if(prefix[mid]==key){
+                return mid;
+            }
+            else if(prefix[mid]<key){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return low-1;
+    }
+    
+    public int minSubArrayLen(int target, int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int[] prefix = new int[nums.length];
+        
+        prefix[0] = nums[0];
+        for(int i=1 ; i<nums.length ; i++){
+            prefix[i] = nums[i]+prefix[i-1];
+        }
+        
+        for(int i=0 ; i<prefix.length ; i++){
+            if(prefix[i]==target){
+                min = Math.min(min , i+1);
+            }
+            else if(prefix[i]>target){
+                int key = prefix[i]-target;
+                int idx = binary_search(prefix , key , i-1);
+                min = Math.min(min , i-idx);
+            }
+        }
+        
+        if(min!=Integer.MAX_VALUE){
+            return min;
+        }
+        return 0;
+    }
+}
