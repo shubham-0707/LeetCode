f848f7da9f11eb70ddfbfd7fec519ff8af9c450b
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-08 23:21:33 +0530

    Time: 2 ms (74.13%), Space: 81.4 MB (16.09%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,22 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // initialising variables..
         
+        //Approach is that we are going to use the Kadane's Algorithm...
         
         int sum = 0;
         int maxi = Integer.MIN_VALUE;
         
         for(int i=0 ; i<nums.length ; i++){
-            sum = sum + nums[i];
-            if(sum>maxi){
-                maxi=sum;
-            }
-            
+            sum+=nums[i];
+            maxi = Math.max(maxi , sum);
             if(sum<0){
-                sum = 0;
+                sum=0;
             }
-            
-            
         }
         return maxi;
     }
