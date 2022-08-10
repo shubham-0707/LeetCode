ee26c025394b4cea7b751bc5ea3808d80f3eff66
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-10 22:53:29 +0530

    Time: 3 ms (91.89%), Space: 45.9 MB (30.93%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,23 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // declaring and initialising the array...
+        
+        // declaring and initialising the answer array...
         int[] ans = new int[2];
-        // declaring the hashmap..
+        
+        //declaring the hashmap for the calculation...
         HashMap<Integer , Integer> hash = new HashMap<>();
         
-        // the approach is that the if the target and target-nums[i] value are present in the hashmap with different indexes then we can say that we have got our answer...
-        
         for(int i=0 ; i<nums.length ; i++){
-            if(hash.containsKey(target-nums[i]) && hash.get(target-nums[i])!=i){
-                ans[0]=i;
-                ans[1]=hash.get(target-nums[i]);
+            int key = target-nums[i];
+            if(hash.containsKey(key) && hash.get(key)!=i){
+                ans[0] = hash.get(key);
+                ans[1] = i;
                 break;
             }
             hash.put(nums[i] , i);
         }
-        
-        // Time Complexity : O(N)
-        // Space Complexity : O(N)
         return ans;
     }
 }
