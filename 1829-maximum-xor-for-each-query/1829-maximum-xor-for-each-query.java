94f3e41339e7bdc2b66ba3c32643b080309577eb
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-13 22:42:57 +0530

    Time: 5 ms (63.45%), Space: 149.2 MB (26.05%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
@@ -2,17 +2,18 @@ class Solution {
     public int[] getMaximumXor(int[] nums, int maximumBit) {
         
         
-        // Using hints I have come to know to a solution...
-        // ans[i] = xor[0-i]^(2^maximumBit-1)
+        // Since, the maximum number that can be formed using maximumBits is 2^maximumBit-1 
         
         int maxi = (int)Math.pow(2 , maximumBit)-1;
-        int[]  ans = new int[nums.length];
-        
         int xor = 0;
+        int[] ans = new int[nums.length];
         for(int i=0 ; i<nums.length ; i++){
             xor=xor^nums[i];
-            ans[nums.length-i-1] = xor^maxi;
+            ans[nums.length-i-1] = xor^maxi; // saving from backward in answer array...
         }
         return ans;
+        
+        // Time Complexity : O(N)
+        // Space Complexity : O(1) if answer array is ignored...
     }
 }
