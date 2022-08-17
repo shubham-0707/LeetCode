05578b36fe070a3b61c95cb522f13f8d6fab4c56
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-17 18:05:34 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (95.22%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        
+        
         int start = 0;
         int end = matrix.length-1;
+        
         int row = -1;
         
         while(start<=end){
-            int mid = start+(end-start)/2;
+            int mid = start + (end-start)/2;
+            
             if(matrix[mid][0]==target){
                 return true;
             }
@@ -16,7 +20,6 @@ class Solution {
             else{
                 end = mid-1;
             }
-            
         }
         
         if(row==-1){
@@ -27,19 +30,17 @@ class Solution {
         end = matrix[0].length-1;
         
         while(start<=end){
-            int mid = start+(end-start)/2;
+            int mid = start + (end-start)/2;
             if(matrix[row][mid]==target){
                 return true;
             }
             else if(matrix[row][mid]<target){
-                start=mid+1;
+                start = mid+1;
             }
             else{
-                end=mid-1;
+                end = mid-1;
             }
         }
-        
         return false;
-        
     }
 }
