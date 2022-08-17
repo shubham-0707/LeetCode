f14fa479ce3b00775c16da39bba7b80b23b33f83
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-17 17:46:51 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (51.61%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,22 +1,16 @@
 class Solution {
-  
     public List<List<Integer>> generate(int n) {
-        //making resultant list..
         
         List<List<Integer>> ans = new ArrayList<>();
         
-        // main operations are performed from here..
-        
         for(int i=1 ; i<=n ; i++){
-            //list to be inserted to the result..
-            List<Integer> temp = new ArrayList<>();
-            // the first element of every line will be 1;
+            List<Integer> arr = new ArrayList<>();
             int C = 1;
             for(int j=1 ; j<=i ; j++){
-                temp.add(C);
+                arr.add(C);
                 C = C*(i-j)/j;
             }
-            ans.add(temp);
+            ans.add(arr);
         }
         return ans;
     }
