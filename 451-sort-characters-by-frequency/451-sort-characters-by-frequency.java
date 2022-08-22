a79c4a7314f69d6abc7b3cc2c819017dd1c77669
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-22 10:52:17 +0530

    Time: 2174 ms (11.45%), Space: 513.4 MB (9.89%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,41 +1,40 @@
 class Solution {
     public String frequencySort(String s) {
         
-        // Sorry guys for this so messy solution...
+        // first we are going to use a hashmap to store the frequency of each element...
         
-        // firstly we will make a hashmap in which we will store the frequency of every chaacter...
         HashMap<Character , Integer> hash = new HashMap<>();
-        
         for(int i=0 ; i<s.length() ; i++){
             hash.put(s.charAt(i) , hash.getOrDefault(s.charAt(i) , 0)+1);
         }
         
-        // then for each frequency we will store the corresponding elements with that frequency in a arraylist in tree map sorted in descending order...
+        // Now I am going to store the characters based on their frequency in an arraylist of character in a treemap sorted in descending order...
         
         TreeMap<Integer , ArrayList<Character>> tree = new TreeMap<>(Collections.reverseOrder());
-        
-        for(char i:hash.keySet()){
-            ArrayList<Character> temp = tree.get(hash.get(i));
+        for(char ch : hash.keySet()){
+            ArrayList<Character> temp = tree.get(hash.get(ch));
+            
             if(temp==null){
-                temp = new ArrayList<Character>();
-                temp.add(i);
-                tree.put(hash.get(i) , temp);
+                temp = new ArrayList<>();
+                temp.add(ch);
+                tree.put(hash.get(ch) , temp);
             }
             else{
-                temp.add(i);
+                temp.add(ch);
             }
         }
         
-        // then for each frequency we will construct our string...
+        // now getting the answer...
         String ans = "";
         for(int i:tree.keySet()){
             for(int j=0 ; j<tree.get(i).size() ; j++){
-                char ch = tree.get(i).get(j);
-                for(int k=0 ; k<i ; k++){
-                    ans=ans+ch;
+                for(int k = 0; k<i ; k++){
+                    ans+=tree.get(i).get(j);
                 }
             }
         }
+        
         return ans;
+        
     }
 }
