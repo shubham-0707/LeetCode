b35bc546db82ad45931ddb2ed7c5f4b8ebd0fa53
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-22 10:43:09 +0530

    Time: 2374 ms (7.19%), Space: 614 MB (5.88%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,11 +1,16 @@
 class Solution {
     public String frequencySort(String s) {
         
+        // Sorry guys for this so messy solution...
+        
+        // firstly we will make a hashmap in which we will store the frequency of every chaacter...
         HashMap<Character , Integer> hash = new HashMap<>();
         
         for(int i=0 ; i<s.length() ; i++){
             hash.put(s.charAt(i) , hash.getOrDefault(s.charAt(i) , 0)+1);
         }
+        
+        // then for each frequency we will store the corresponding elements with that frequency in a arraylist in tree map sorted in descending order...
         
         TreeMap<Integer , ArrayList<Character>> tree = new TreeMap<>(Collections.reverseOrder());
         
@@ -21,6 +26,7 @@ class Solution {
             }
         }
         
+        // then for each frequency we will construct our string...
         String ans = "";
         for(int i:tree.keySet()){
             for(int j=0 ; j<tree.get(i).size() ; j++){
