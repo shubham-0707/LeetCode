7fb694e387a484855081400bef2e002fb2074f71
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-22 11:30:39 +0530

    Time: 22 ms (18.26%), Space: 49.8 MB (5.18%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -1,7 +1,8 @@
 class Solution {
     public String intToRoman(int num) {
-        LinkedHashMap<Integer , String> hash = new LinkedHashMap<>();
         
+        
+        LinkedHashMap<Integer , String> hash = new LinkedHashMap<>();
         hash.put(1000 , "M");
         hash.put(900 , "CM");
         hash.put(500 , "D");
@@ -12,19 +13,17 @@ class Solution {
         hash.put(40 , "XL");
         hash.put(10 , "X");
         hash.put(9 , "IX");
-        hash.put(5 , "V");
+        hash.put(5 ,"V");
         hash.put(4 , "IV");
         hash.put(1 , "I");
         
-        
-        StringBuilder str = new StringBuilder();
+        String ans = "";
         for(Map.Entry<Integer , String> i : hash.entrySet()){
-            while(num >=i.getKey()){
-                str.append(i.getValue());
+            while(num>=i.getKey()){
+                ans = ans + i.getValue();
                 num-=i.getKey();
             }
         }
-        
-        return str.toString();
+        return ans;
     }
 }
