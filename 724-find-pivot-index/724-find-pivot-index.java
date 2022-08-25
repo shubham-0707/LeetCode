1fdb11930500673fa8f96388c4458a431839037a
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-25 19:58:24 +0530

    Time: 1 ms (100.00%), Space: 42.9 MB (95.50%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,11 +1,13 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         
+        // initialising and calculating the sum
         int sum = 0;
         for(int i=0 ; i<nums.length ; i++){
             sum+=nums[i];
         }
         
+        // initialising and calculating the prefix Sum...
         int prevSum = 0;
         for(int i=0 ; i<nums.length ; i++){
             if(prevSum == (sum-prevSum-nums[i])){
@@ -14,6 +16,8 @@ class Solution {
             prevSum+=nums[i];
         }
         
+        // Time Complexity : O(N)
+        // Space Complexity : O(1)
         return -1;
     }
 }
