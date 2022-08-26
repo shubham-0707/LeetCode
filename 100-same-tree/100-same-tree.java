33d6b93af9dfc58926e3336c2ff5203886b1b708
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-26 22:13:12 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (8.41%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -15,17 +15,16 @@
  */
 class Solution {
     
-    
-    public static boolean recur(TreeNode p , TreeNode q){
-        if(p==null  && q==null)
-            return true;
-        if(p==null || q==null)
-            return false;
-        if(p.val!=q.val) return false;
-        return recur(p.left , q.left) && recur(p.right , q.right);
+    public boolean recur(TreeNode root1 , TreeNode root2){
+        if(root1==null && root2==null) return true;
+        if(root1==null || root2==null) return false;
+        
+        return root1.val==root2.val && recur(root1.left , root2.left) && recur(root1.right , root2.right);
+        
     }
     
     public boolean isSameTree(TreeNode p, TreeNode q) {
         return recur(p , q);
+        
     }
 }
