1a055e607ee7c62994ae4ddd86dd4b6bbbceafd8
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-26 21:54:02 +0530

    Time: 2 ms (64.56%), Space: 50.1 MB (79.23%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,13 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) {
         
-        // Because of two same values gives us zero thats why only the element which occurs once is left at the last...
+        // There can be many approaches to solve this question...
+        // I am going to solve this using the XOR method...
         
         int xor = 0;
         for(int i=0 ; i<nums.length ; i++){
             xor = xor^nums[i];
         }
-        
         return xor;
         
         // Time Complexity : O(N)
