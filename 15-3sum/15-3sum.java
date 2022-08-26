6371ccee4cdfd21d13ab0bd83008504c127b1909
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-08-27 01:06:01 +0530

    Time: 1012 ms (9.64%), Space: 47.3 MB (85.72%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,10 +1,14 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
         
+        // sorting the array to reduce the loop complexity...
         Arrays.sort(arr);
         
+        // using hashset to remove the duplicate values...
         HashSet<List<Integer>> set = new HashSet<>();
         
+        
+        // traversing from 0 to array length...
         for(int i=0 ; i<arr.length ; i++){
             HashMap<Integer , Integer> hash = new HashMap<>();
             
@@ -25,14 +29,17 @@ class Solution {
                 }
                 
                 hash.put(arr[j] , j);
-                
             }
         }
         
+        // Adding all the set elements to a 2D list to return the answer...
         List<List<Integer>> ans = new ArrayList<>();
         ans.addAll(set);
         
         return ans;
+        
+        // Time Complexity : O(N^2)
+        // Space Complexity : O(N)
         
     }
 }
