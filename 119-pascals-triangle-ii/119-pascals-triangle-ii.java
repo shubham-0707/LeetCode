699f78c0bb07b0e797f7355b9757ab7a05b1ba77
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-01 15:34:00 +0530

    Time: 2 ms (59.25%), Space: 41.6 MB (70.13%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -1,12 +1,11 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
         
-        int MOD = 1000000009;
         List<List<Integer>> ans = new ArrayList<>();
         
         for(int i=1 ; i<=rowIndex+1 ; i++){
-            List<Integer> temp = new ArrayList<>();
             long C = 1;
+            List<Integer> temp = new ArrayList<>();
             for(int j=1 ; j<=i ; j++){
                 temp.add((int)C);
                 C = C*(i-j)/j;
@@ -16,5 +15,7 @@ class Solution {
         
         return ans.get(rowIndex);
         
+        // Time Complexity : O(N^2);
+        // Space Complexity : O(N)
     }
 }
