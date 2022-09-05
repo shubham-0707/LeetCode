f8cad49a1a7240a168378bf59eb7735addd84cdc
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-05 19:39:25 +0530

    Time: 429 ms (22.98%), Space: 0B (100.00%) - LeetHub

--- 595-big-countries/595-big-countries.sql
+++ 595-big-countries/595-big-countries.sql
@@ -1,2 +1,3 @@
-# Write your MySQL query statement below
-select name , population , area from world where area>=3000000 or population>=25000000;
+# Write your MySQL query statement below....
+
+SELECT NAME , POPULATION , AREA FROM WORLD WHERE AREA>=3000000 OR POPULATION>=25000000;
