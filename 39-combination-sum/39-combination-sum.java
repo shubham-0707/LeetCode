1ef882180160f0cbc8bc342f1400561bdbd8850a
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-08 19:56:24 +0530

    Time: 6 ms (61.11%), Space: 45.8 MB (31.34%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,35 +1,39 @@
 class Solution {
-    public static void comb_Sum(int idx , int[] arr , List<Integer> temp , List<List<Integer>> ans , int s , int sum){
-        if(sum<0){
+    
+    public static void comb_sum(int idx , int[] arr , List<List<Integer>> ans , List<Integer> temp , int sum , int target){
+
+        if(target<0){
             return;
         }
+        
         if(idx==arr.length){
-            if(sum==0){
+            if(target==0){
                 ans.add(new ArrayList<>(temp));
             }
             return;
         }
         
-        // picking
-        s+=arr[idx];
+        // picking case..
+        sum+=arr[idx];
         temp.add(arr[idx]);
-        comb_Sum(idx , arr , temp , ans , s , sum-arr[idx]);
+        comb_sum(idx, arr , ans , temp , sum , target-arr[idx]);
         
-        
-        // not picking
-        s-=arr[idx];
+        // not picking...
+        sum-=arr[idx];
         temp.remove(temp.size()-1);
-        comb_Sum(idx+1 , arr , temp , ans , s , sum);
-        
+        comb_sum(idx+1 , arr , ans , temp , sum , target);
     }
     
+    
     public List<List<Integer>> combinationSum(int[] arr , int target) {
-        List<Integer> temp = new ArrayList<>();
+        
         List<List<Integer>> ans = new ArrayList<>();
         
-        comb_Sum(0 , arr , temp , ans , 0 , target);
+        List<Integer> temp = new ArrayList<>();
+        
+        comb_sum(0 , arr , ans , temp , 0 , target);
         
         return ans;
-               
+        
     }
 }
