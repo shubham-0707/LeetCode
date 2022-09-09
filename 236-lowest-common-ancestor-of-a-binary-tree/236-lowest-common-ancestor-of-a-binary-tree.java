34ff5f199800337903cc56ac5057481c5ecf7acb
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-09 15:35:38 +0530

    Time: 58 ms (5.01%), Space: 50 MB (5.07%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,49 +9,40 @@
  */
 class Solution {
     
-    public boolean getPath(TreeNode root , TreeNode target , ArrayList<TreeNode> arr){
+    public boolean getPaths(ArrayList<TreeNode> temp , TreeNode root , TreeNode target){
         if(root==null) return false;
-        arr.add(root);
-        if(root==target) return true;
         
-        
-        if(getPath(root.left , target , arr) || getPath(root.right , target , arr)){
+        temp.add(root);
+        if(root==target){
+            return true;
+        }
+        if(getPaths(temp , root.left  , target) || getPaths(temp , root.right , target)){
             return true;
         }
         
-        arr.remove(arr.size()-1);
-        
+        temp.remove(temp.size()-1);
         return false;
-        
         
     }
     
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> ans1 = new ArrayList<>();
-        ArrayList<TreeNode> ans2 = new ArrayList<>();
         
-        getPath(root , p , ans1);
-        getPath(root , q , ans2);
+        ArrayList<TreeNode> arr1 = new ArrayList<>();
+        ArrayList<TreeNode> arr2 = new ArrayList<>();
         
-        for(int i=0 ; i<ans1.size() ; i++){
-            System.out.print(ans1.get(i).val+" ");
-        }
-        
-        for(int i=0 ; i<ans2.size() ; i++){
-            System.out.print(ans2.get(i).val+" ");
-        }
+        getPaths(arr1 , root , p);
+        getPaths(arr2 , root , q);
         
         HashMap<TreeNode , Integer> hash = new HashMap<>();
-        
-        for(int i =0 ; i<ans1.size() ; i++){
-            hash.put(ans1.get(i) , hash.getOrDefault(ans1.get(i) , 0)+1);
+        for(int i=0 ; i<arr1.size() ; i++){
+            hash.put(arr1.get(i) , hash.getOrDefault(arr1.get(i) , 0)+1);
         }
         TreeNode ans = root;
-        for(int i=0 ; i<ans2.size() ; i++){
-            hash.put(ans2.get(i) , hash.getOrDefault(ans2.get(i) , 0)+1);
+        for(int i=0 ; i<arr2.size() ; i++){
+            hash.put(arr2.get(i) , hash.getOrDefault(arr2.get(i) , 0)+1);
             
-            if(hash.get(ans2.get(i))==2){
-                ans=ans2.get(i);
+            if(hash.get(arr2.get(i))==2){
+                ans=arr2.get(i);
             }
         }
         
