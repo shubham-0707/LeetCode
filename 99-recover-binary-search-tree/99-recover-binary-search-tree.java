f37efde8636bf7cc81f1910d94374a324591687d
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-10 23:18:16 +0530

    Time: 4 ms (46.09%), Space: 47 MB (74.93%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -15,54 +15,50 @@
  */
 class Solution {
     
-    public void inorder(TreeNode root , TreeMap<Integer , TreeNode> tree , LinkedHashMap<Integer , TreeNode> hash){
+    private TreeNode prev;
+    private TreeNode first;
+    private TreeNode middle;
+    private TreeNode last;
+    
+    
+    public void inorder(TreeNode root){
         if(root==null) return;
         
-        inorder(root.left , tree , hash);
-        tree.put(root.val  , root);
-        hash.put(root.val , root);
-        inorder(root.right , tree , hash);
+        inorder(root.left);
+        
+        if(root.val<prev.val){
+            if(first==null){
+                first=prev;
+                middle = root;
+            }
+            else{
+                last = root;
+            }
+        }
+        
+        prev=root;
+        
+        inorder(root.right);
+        
     }
-     
+    
     
     public void recoverTree(TreeNode root) {
-        TreeNode first = null;
-        TreeNode second = null;
+        prev = new TreeNode(Integer.MIN_VALUE);
+        first = middle = last = null;
         
-        TreeMap<Integer , TreeNode> tree = new TreeMap<>();
-        LinkedHashMap<Integer , TreeNode> hash = new LinkedHashMap<>();
+        inorder(root);
         
-        inorder(root , tree , hash);
-        
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i:hash.keySet()){
-            arr.add(i);
+        if(first!=null && last!=null){
+            int v = first.val;
+            first.val = last.val;
+            last.val = v;
         }
-        
-        // for(int i : hash.keySet()){
-        //     System.out.print(i+" ");
-        // }
-        // System.out.println();
-        // for(int i : tree.keySet()){
-        //     System.out.print(i+" ");
-        // }
-        
-        
-        
-        int x = 0;
-        for(int i : tree.keySet()){
-            if(first==null && i!=arr.get(x)){
-                first = hash.get(i);
-            }
-            else if(first!=null && i!=arr.get(x)){
-                second = hash.get(i);
-            }
-            x++;
+        else if(first!=null && middle!=null){
+            int v = first.val;
+            first.val = middle.val;
+            middle.val = v;
         }
-        
-        int v = first.val;
-        first.val = second.val;
-        second.val = v;
         
     }
 }
