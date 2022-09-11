0398f589ecd92891cf9d8f8c6ac0c40014e64eea
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-11 20:50:19 +0530

    Time: 1016 ms (9.95%), Space: 47.6 MB (85.62%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,45 +1,32 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] arr) {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         
-        // sorting the array to reduce the loop complexity...
-        Arrays.sort(arr);
+        Set<List<Integer>> set = new HashSet<>();
         
-        // using hashset to remove the duplicate values...
-        HashSet<List<Integer>> set = new HashSet<>();
-        
-        
-        // traversing from 0 to array length...
-        for(int i=0 ; i<arr.length ; i++){
+        for(int i=0 ; i<nums.length ; i++){
             HashMap<Integer , Integer> hash = new HashMap<>();
-            
-            for(int j=i+1 ; j<arr.length ; j++){
+            for(int j=i+1 ; j<nums.length ; j++){
                 
-                if(hash.containsKey(0-arr[i]-arr[j])){
-                    int k = hash.get(0-arr[i]-arr[j]);
+                if(hash.containsKey(0-nums[i]-nums[j])){
+                    int k = hash.get(0-nums[i]-nums[j]);
                     
                     if(k==i || k==j) continue;
                     
                     else{
-                        ArrayList<Integer> temp = new ArrayList<>();
-                        temp.add(arr[i]);
-                        temp.add(arr[j]);
-                        temp.add(arr[k]);
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
                         set.add(temp);
                     }
                 }
-                
-                hash.put(arr[j] , j);
+                hash.put(nums[j] , j);
             }
         }
         
-        // Adding all the set elements to a 2D list to return the answer...
-        List<List<Integer>> ans = new ArrayList<>();
-        ans.addAll(set);
-        
-        return ans;
-        
-        // Time Complexity : O(N^2)
-        // Space Complexity : O(N)
-        
+        List<List<Integer>> list = new ArrayList<>();
+        list.addAll(set);
+        return list;
     }
 }
