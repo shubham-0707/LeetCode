ebfa1b724162ae2e5af58f782e2c0f83a3f1c0dd
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-13 17:49:42 +0530

    Time: 41 ms (5.03%), Space: 48.2 MB (14.03%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,43 +9,44 @@
  */
 class Solution {
     
-    public boolean getPaths(ArrayList<TreeNode> temp , TreeNode root , TreeNode target){
+    public boolean getPath(TreeNode root , List<TreeNode> arr , TreeNode target){
         if(root==null) return false;
         
-        temp.add(root);
-        if(root==target){
-            return true;
-        }
-        if(getPaths(temp , root.left  , target) || getPaths(temp , root.right , target)){
+        arr.add(root);
+        
+        if(root==target) return true;
+        
+        if(getPath(root.left , arr , target) || getPath(root.right , arr , target)){
             return true;
         }
         
-        temp.remove(temp.size()-1);
+        arr.remove(arr.size()-1);
+        
         return false;
-        
     }
     
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-        ArrayList<TreeNode> arr1 = new ArrayList<>();
-        ArrayList<TreeNode> arr2 = new ArrayList<>();
+        List<TreeNode> arr1 = new ArrayList<>();
+        List<TreeNode> arr2 = new ArrayList<>();
         
-        getPaths(arr1 , root , p);
-        getPaths(arr2 , root , q);
+        getPath(root , arr1 , p);
+        getPath(root , arr2 , q);
         
         HashMap<TreeNode , Integer> hash = new HashMap<>();
         for(int i=0 ; i<arr1.size() ; i++){
             hash.put(arr1.get(i) , hash.getOrDefault(arr1.get(i) , 0)+1);
         }
+        
         TreeNode ans = root;
-        for(int i=0 ; i<arr2.size() ; i++){
+        
+        for(int i=0 ; i<arr2.size(); i++){
             hash.put(arr2.get(i) , hash.getOrDefault(arr2.get(i) , 0)+1);
             
             if(hash.get(arr2.get(i))==2){
-                ans=arr2.get(i);
+                ans = arr2.get(i);
             }
         }
-        
         return ans;
     }
 }
