e9137c5f4676b5093602c88b037930fcdcaf21cc
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-17 22:34:46 +0530

    Time: 2 ms (84.09%), Space: 55 MB (35.31%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,24 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         
-        /*
-        Approach : In this approach we are swapping the starting element of the array with the non zero elements and incrementing the pointer...
-        */
-        
-        int j=0;
+        int x = 0;
         for(int i=0 ; i<nums.length ; i++){
             if(nums[i]!=0){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
+                nums[x] = nums[i];
+                x++;
             }
         }
-        for(;j<nums.length ; j++){
-            nums[j]=0;
+        
+        for(int i=x ; i<nums.length ; i++){
+            nums[i]=0;
         }
         
-        // Time Complexity : O(n)
-        // Space Complexity : O(1) (in place)
     }
 }
