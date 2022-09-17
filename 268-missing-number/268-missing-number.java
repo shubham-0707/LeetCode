50718856a4e718fe46dac612693573e38a584c06
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-17 23:09:09 +0530

    Time: 25 ms (5.23%), Space: 54.2 MB (5.78%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,21 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        
-        // Here we are using a hashmap approach to insert all the elements of the array nums..
         HashMap<Integer , Integer> hash = new HashMap<>();
         
         for(int i=0 ; i<nums.length ; i++){
             hash.put(nums[i] , hash.getOrDefault(nums[i] , 0)+1);
         }
         
-                // if any element from 0 to n-1 is not present in the hashmap then simply return that number...
-        for(int i=0 ; i<nums.length ; i++){
-            if(!hash.containsKey(i)){
-                return i;
-            }
+        for(int i=0 ; i<=nums.length ; i++){
+            if(!hash.containsKey(i)) return i;
         }
         
-        // or return the length of the array as length==n..
-        return nums.length;
+        return 0;
     }
 }
