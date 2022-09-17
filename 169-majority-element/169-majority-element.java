0dcc943e5608e44a9ab077b1832bc8b4d9d95c4c
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-17 21:39:38 +0530

    Time: 25 ms (13.92%), Space: 55.9 MB (56.23%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,26 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        
-        // use hashmap to store the frequency of each element...
-        
         HashMap<Integer , Integer> hash = new HashMap<>();
+        
         for(int i=0 ; i<nums.length ; i++){
-            hash.put(nums[i] , hash.getOrDefault(nums[i] , 0)+1);
+            hash.put(nums[i] , hash.getOrDefault(nums[i] ,0)+1);
         }
         
-        // checking the majority element and updating the answer variable...
-        int ans = 0;
         for(int i:hash.keySet()){
-            if(hash.get(i)>(nums.length/2)){
-                ans=i;
-                break;
+            if(hash.get(i)>nums.length/2){
+                return i;
             }
         }
         
-        return ans;
-        
-        // Time Complexity : O(N)
-        // Space Complexity : O(N)
-        
+        return 0;
     }
 }
