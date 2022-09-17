32b95cb2c98dc1baee5293a48fa14d87a4244b6a
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-17 21:37:38 +0530

    Time: 3 ms (49.82%), Space: 50.8 MB (43.35%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,16 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        
-        // There can be many approaches to solve this question...
-        // I am going to solve this using the XOR method...
-        
         int xor = 0;
         for(int i=0 ; i<nums.length ; i++){
             xor = xor^nums[i];
         }
         return xor;
-        
-        // Time Complexity : O(N)
-        // Space Complexity : O(1)
     }
 }
