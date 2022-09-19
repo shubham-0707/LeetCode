93b164296c9a1c30620dcb0805564f532fddbec3
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-19 22:27:33 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (19.25%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,26 +1,28 @@
 class Solution {
-    
-    
-    
     public void rotate(int[][] matrix) {
         
-        //swapping values..
+        
+        // swapping...
+        
         for(int i=0 ; i<matrix.length ; i++){
-            for(int j=i+1 ; j<matrix.length ; j++){
+            for(int j=i+1 ; j<matrix[i].length ; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
         
-        //rotation by 90 degree..
+        // rotating...
+        
         for(int i=0 ; i<matrix.length ; i++){
             int start = 0;
-            int end = matrix.length-1;
+            int end = matrix[i].length-1;
+            
             while(start<end){
                 int temp = matrix[i][start];
                 matrix[i][start] = matrix[i][end];
                 matrix[i][end] = temp;
+                
                 start++;
                 end--;
             }
