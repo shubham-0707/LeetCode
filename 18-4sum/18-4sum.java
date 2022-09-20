7f5b5aaa74e1d69b5b9ec088c24d09c8b2f1f382
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-20 22:28:11 +0530

    Time: 59 ms (23.42%), Space: 44.1 MB (80.86%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -2,17 +2,27 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
         Arrays.sort(nums);
+        
         for(int i=0 ; i<nums.length ; i++){
+            
             for(int j=i+1 ; j<nums.length ; j++){
+                
                 int k=j+1;
                 int l=nums.length-1;
                 while(k<l){
                     int c = nums[k];
+                    
                     int d = nums[l];
-                    int sum = nums[i]+nums[j]+nums[k]+nums[l];
-                    List<Integer> arr = new ArrayList<Integer>();
-                    if(sum==target){
-                        
+                    
+                    int sum1 = nums[i]+ nums[l];
+                    int sum2 = nums[j] + nums[k];
+                    int sum = sum1 + sum2;
+                    if (sum1 + sum2 == target && ((sum1 > Integer.MAX_VALUE - sum2 && sum2 > 0)
+                            || (sum1 < Integer.MIN_VALUE - sum2 && sum2 < 0))){
+                        return ans;
+                    }
+                    else if(sum1+sum2==target){
+                        List<Integer> arr = new ArrayList<Integer>();
                             arr.add(nums[i]);
                             arr.add(nums[j]);
                             arr.add(nums[k]);
@@ -24,7 +34,7 @@ class Solution {
                         
                     }
                     
-                    else if(sum<target){
+                    else if(sum1+sum2<target){
                         k++;
                         while(k<l && nums[k]==c) k++;
                     }
