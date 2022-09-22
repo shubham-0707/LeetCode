7dd7a5d1d436042576ed00b2fbe4aa6f377126f8
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-22 20:47:04 +0530

    Time: 33 ms (11.52%), Space: 46.5 MB (15.62%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -2,13 +2,12 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         
         if(s.length()!=t.length()) return false;
-        // we are going to use 2 hashmaps to solve this question...
         
         HashMap<Character , Integer> hash1 = new HashMap<>();
         HashMap<Character , Integer> hash2 = new HashMap<>();
         
-        for(int i= 0 ; i<s.length() ; i++){
-            hash1.put(s.charAt(i), hash1.getOrDefault(s.charAt(i) , 0)+1);
+        for(int i=0 ; i<s.length() ; i++){
+            hash1.put(s.charAt(i) , hash1.getOrDefault(s.charAt(i) , 0)+1);
         }
         
         for(int i=0 ; i<t.length() ; i++){
