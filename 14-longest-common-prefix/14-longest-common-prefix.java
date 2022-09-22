ee69f0310ba262c1d69bf120b88cfde6bf31099f
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-22 19:47:26 +0530

    Time: 14 ms (16.12%), Space: 42.8 MB (17.95%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,6 +1,8 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         
+        
+        // Firstly I have found the minimum sized string because the answer cannot exceed the length of a minimum length string in strs...
         int mini = Integer.MAX_VALUE;
         int minIndex = 0;
         for(int i=0 ; i<strs.length ; i++){
@@ -10,6 +12,7 @@ class Solution {
             }
         }
         
+        // Then I have checked for every character of the test string in the given string array...
         String test = strs[minIndex];
         String ans = "";
         for(int i=0 ; i<test.length() ; i++){
@@ -21,5 +24,8 @@ class Solution {
             ans+=test.charAt(i);
         }
         return ans;
+        
+        // Time Complexity : O(N^2)
+        // Space Complexity : O(1)
     }
 }
