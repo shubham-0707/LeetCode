45650dba11d6a223cfc27ae1bbb29b31bfc8c301
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-24 01:04:44 +0530

    Time: 13 ms (32.46%), Space: 46.8 MB (29.56%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -2,17 +2,18 @@ class Solution {
     public int romanToInt(String s) {
         
         
+        int sum = 0;
         HashMap<Character , Integer> hash = new HashMap<>();
         
         hash.put('I' , 1);
-        hash.put('V' , 5);
-        hash.put('X' , 10);
+        hash.put('V' ,5);
+        hash.put('X' ,10);
         hash.put('L' , 50);
         hash.put('C' , 100);
         hash.put('D' , 500);
         hash.put('M' , 1000);
         
-        int sum = hash.get(s.charAt(s.length()-1));
+        sum = hash.get(s.charAt(s.length()-1));
         
         for(int i=s.length()-2 ; i>=0 ; i--){
             if(hash.get(s.charAt(i))>=hash.get(s.charAt(i+1))){
