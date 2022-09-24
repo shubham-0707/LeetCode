72dc238d62f234579360afaad9a57595b1506bc2
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-24 16:29:49 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (90.64%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,19 @@
 class Solution {
     
-    public static int binarySearch1(int[] nums , int target){
+    public static int binarySearch2(int[] arr , int target){
         int ans = -1;
         
         int start = 0;
-        int end = nums.length-1;
+        int end = arr.length-1;
         
         while(start<=end){
             int mid = start + (end-start)/2;
             
-            if(nums[mid]==target){
+            if(arr[mid]==target){
                 ans=mid;
-                end=mid-1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-        }
-        return ans;
-    }
-    
-    public static int binarySearch2(int[] nums , int target){
-        int ans = -1 ;
-        int start = 0;
-        int end = nums.length-1;
-        
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            
-            if(nums[mid]==target){
-                ans = mid;
                 start = mid+1;
             }
-            else if(nums[mid]<target){
+            else if(arr[mid]<target){
                 start = mid+1;
             }
             else{
@@ -44,12 +22,31 @@ class Solution {
         }
         return ans;
     }
-    public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[2];
+    
+    public static int binarySearch1(int[] arr , int target){
+        int ans = -1;
         
-        ans[0] = binarySearch1(nums , target);
-        ans[1] = binarySearch2(nums , target);
+        int start = 0;
+        int end = arr.length-1;
+        
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            
+            if(arr[mid]==target){
+                ans = mid;
+                end = mid-1;
+            }
+            else if(arr[mid]<target) start=mid+1;
+            else end = mid-1;
+        }
         
         return ans;
+    }
+    
+    public int[] searchRange(int[] nums, int target) {
+        int ans1 = binarySearch1(nums , target);
+        int ans2 = binarySearch2(nums , target);
+        
+        return new int[]{ans1 , ans2};
     }
 }
