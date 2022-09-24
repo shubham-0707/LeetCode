956115e5a34b1d480df39fa24f4b2c55eeddc2fd
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-24 22:40:32 +0530

    Time: 1 ms (78.16%), Space: 42.1 MB (84.47%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,14 +1,13 @@
 class Solution {
     
-    public static int BinarySearch(int[] arr , int target , int start , int end){
-        
-        
+    public int binarySearch(int[] nums , int target , int start , int end){
         while(start<=end){
             int mid = start + (end-start)/2;
-            if(arr[mid]==target){
+            
+            if(nums[mid]==target){
                 return mid;
             }
-            else if(arr[mid]<target){
+            else if(nums[mid]<target){
                 start = mid+1;
             }
             else{
@@ -18,34 +17,33 @@ class Solution {
         return -1;
     }
     
-    public static int findPivot(int[] arr){
-    int start = 0;
-    int end = arr.length-1;
-    int mid = start + (end-start)/2;
-    
-    while(start<end){
-        if(arr[mid]>=arr[0]){
-            start = mid+1;
+    public int findPivot(int[] nums){
+        int start = 0;
+        int end = nums.length-1;
+        int mid = start + (end-start)/2;
+        while(start<end){
+            if(nums[0]<=nums[mid]){
+                start = mid+1;
+            }
+            else{
+                end = mid;
+            }
+            mid = start + (end-start)/2;
         }
-        else{
-            end = mid;
-        }
-        mid = start + (end-start)/2;
+        return mid;
     }
-    return mid;
-}
     
     public int search(int[] nums, int target) {
-        int idx = findPivot(nums);
         
+        int idx = findPivot(nums);
+        //System.out.println(idx+" ");
         int ans = 0;
         if(target>=nums[idx] && target<=nums[nums.length-1]){
-            ans = BinarySearch(nums , target , idx , nums.length-1);
+            ans = binarySearch(nums , target , idx , nums.length-1);
         }
         else{
-            ans = BinarySearch(nums , target , 0 , idx-1);
+            ans = binarySearch(nums , target , 0 , idx-1);
         }
-        
         return ans;
     }
 }
