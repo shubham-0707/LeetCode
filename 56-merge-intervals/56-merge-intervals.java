393477b195685b298189e18dae081c4537455745
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-28 20:19:42 +0530

    Time: 59 ms (5.04%), Space: 55.1 MB (66.39%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,10 +1,14 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         
+        // Sorting the intervals array based on the first value of the intenal arrays of size 2...
         Arrays.sort(intervals , (a , b)->(a[0]-b[0]));
         
+        // forming a linked list
         LinkedList<int[]> list = new LinkedList<>();
         
+        
+        // applying the logic to merge the intervals...
         for(int[] i : intervals){
             if(list.isEmpty() || list.get(list.size()-1)[1]<i[0]){
                 list.add(i);
@@ -14,6 +18,8 @@ class Solution {
             }
         }
         
+        
+        // inserting the intervals from linked list to the answer array....
         int[][] ans = new int[list.size()][2];
         
         for(int i=0 ; i<list.size() ; i++){
@@ -21,6 +27,9 @@ class Solution {
         }
         
         return ans;
+        
+        // Time Complexity : O(n*logn)
+        // Space Complexity : O(n)
         
     }
 }
