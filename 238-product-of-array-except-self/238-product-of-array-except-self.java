5bb6161d36b0f5333d11cb40801bc6689d95d461
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-09-28 20:20:12 +0530

    Time: 3 ms (62.07%), Space: 57 MB (83.61%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,22 +1,30 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        // The intution to solve this question is that if we make a prefix product array and a suffix product array to calculate the answer array..
+        
         int[] ans = new int[nums.length];
-        int[] prefixProd = new int[nums.length];
-        int[] suffixProd = new int[nums.length];
+        int[] prefix_prod = new int[nums.length];
+        int[] suffix_prod = new int[nums.length];
+        
         int prod = 1;
         for(int i=0 ; i<nums.length ; i++){
-            prefixProd[i] = prod;
+            prefix_prod[i] = prod;
             prod = prod*nums[i];
         }
+        
         prod = 1;
         for(int i=nums.length-1 ; i>=0 ; i--){
-            suffixProd[i] = prod;
+            suffix_prod[i] = prod;
             prod = prod*nums[i];
         }
         
         for(int i=0 ; i<nums.length ; i++){
-            ans[i] = suffixProd[i]*prefixProd[i];
+            ans[i] = prefix_prod[i]*suffix_prod[i];
         }
+        
         return ans;
+        
+        // Time Complexity : O(N)
+        // Space Complexity : O(N)
     }
 }
