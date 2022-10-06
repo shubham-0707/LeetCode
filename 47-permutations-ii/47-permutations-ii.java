3cafeb157b4f6c3d5f5a37436a6008eacf94914f
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-10-06 20:23:07 +0530

    Time: 75 ms (17.79%), Space: 54 MB (16.37%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,28 +1,28 @@
 class Solution {
     
-    public static void permutation(int[] nums , List<Integer> ds , HashSet<List<Integer>> set , boolean[] hash){
-        if(ds.size()==nums.length){
-            set.add(new ArrayList<>(ds));
+    public static void solve(int[] nums , List<Integer> temp , Set<List<Integer>> set , boolean[] arr){
+        if(temp.size()==nums.length){
+            set.add(new ArrayList<>(temp));
             return;
         }
         
         for(int i=0 ; i<nums.length ; i++){
-            if(!hash[i]){
-                hash[i]=true;
-                ds.add(nums[i]);
-                permutation(nums , ds , set , hash);
-                ds.remove(ds.size()-1);
-                hash[i]=false;
+            if(!arr[i]){
+                arr[i] = true;
+                temp.add(nums[i]);
+                solve(nums , temp , set ,  arr);
+                temp.remove(temp.size()-1);
+                arr[i] = false;
             }
         }
     }
     
+    
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<Integer> ds = new ArrayList<>();
-        HashSet<List<Integer>> set = new HashSet<>();
-        boolean[] hash = new boolean[nums.length];
-        permutation(nums  , ds , set , hash);
-        
+        Set<List<Integer>> set = new HashSet<>();
+        List<Integer> temp = new ArrayList<>();
+        boolean[] arr = new boolean[nums.length];
+        solve(nums , temp , set , arr);
         List<List<Integer>> ans = new ArrayList<>();
         ans.addAll(set);
         return ans;
