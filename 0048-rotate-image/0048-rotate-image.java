7785159055810521d88b37ba0cf0daa9ee422d2f
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-10-11 01:10:14 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (67.97%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        
+        // swapping...
+        
+        for(int i=0 ; i<matrix.length ; i++){
+            for(int j=i+1 ; j<matrix[i].length ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // rotating...
+        
+        for(int i=0 ; i<matrix.length ; i++){
+            int start = 0;
+            int end = matrix[i].length-1;
+            
+            while(start<end){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                
+                start++;
+                end--;
+            }
+        }
+        
+    }
+}
