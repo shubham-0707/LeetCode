75467a53448224e0120598f0e1331d7646de0df1
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-10-13 22:35:34 +0530

    Time: 13 ms (29.88%), Space: 43.2 MB (54.48%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String addStrings(String str1, String str2) {
+        
+        int i = str1.length()-1;
+        int j = str2.length()-1;
+        
+        String ans = "";
+        int carry = 0;
+        while(i>=0 || j>=0){
+           int sum = carry;
+            
+            if(i>=0){
+                sum+=Character.getNumericValue(str1.charAt(i));
+            }
+            
+            if(j>=0){
+                sum+=Character.getNumericValue(str2.charAt(j));
+            }
+            
+            int value = sum%10;
+            
+            char ch = Integer.toString(value).charAt(0);
+            
+            ans = ch+ans;
+            
+            carry = sum/10;
+            
+            i--;
+            j--;
+            
+        }
+        if(carry>0)
+            ans = Integer.toString(carry).charAt(0) + ans;
+            
+        return ans;
+        
+        
+        // Time Complexity : O(N)
+        // Space Complexity : O(1)
+    }
+}
