901d9d3b66b5ead9afaddc3e0099d896b7b74413
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-11-08 23:54:51 +0530

    Time: 1 ms (98.41%), Space: 39.8 MB (97.89%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,28 +1,29 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
+        String[] string = s.split(" ");
         
-        String[] arr = s.split(" ");
-        if(pattern.length()!=arr.length) return false;
+        
+        if(string.length!=pattern.length()) return false;
         
         HashMap<Character , String> hash = new HashMap<>();
         
         for(int i=0 ; i<pattern.length() ; i++){
-            if(hash.containsKey(pattern.charAt(i)) && hash.get(pattern.charAt(i)).equals(arr[i])==false){
+            if(hash.containsKey(pattern.charAt(i)) && hash.get(pattern.charAt(i)).equals(string[i])==false){
                 return false;
             }
             else{
-                hash.put(pattern.charAt(i) , arr[i]);
+                hash.put(pattern.charAt(i) , string[i]);
             }
         }
         
         HashMap<String , Character> hash2 = new HashMap<>();
         
-        for(int i=0 ; i<arr.length ; i++){
-            if(hash2.containsKey(arr[i]) && hash2.get(arr[i])!=pattern.charAt(i)){
+        for(int i=0 ; i<string.length ; i++){
+            if(hash2.containsKey(string[i]) && hash2.get(string[i]).equals(pattern.charAt(i))==false){
                 return false;
             }
             else{
-                hash2.put(arr[i] , pattern.charAt(i));
+                hash2.put(string[i] , pattern.charAt(i));
             }
         }
         
@@ -30,5 +31,6 @@ class Solution {
         
         // Time Complexity : O(N)
         // Space Complexity : O(N)
+        
     }
 }
