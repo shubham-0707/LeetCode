a37d4f0e7bfd545fa2ad23a66d39396086a68a9b
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-11-10 19:27:50 +0530

    Time: 38 ms (61.93%), Space: 43.1 MB (54.32%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,64 +1,36 @@
 class Solution {
-
+    int maxi = 1;
+    int[] temp = new int[2];
+    public void findStartEnd(String s , int l , int r){
+        
+        while(l>=0 && r<s.length()){
+            if(s.charAt(l)==s.charAt(r)){
+                l--;
+                r++;
+            }
+            else{
+                break;
+            }
+        }
+        
+        if(r-l-1>maxi){
+            maxi = r-l-1;
+            temp[0] = l+1;
+            temp[1] = r-1;
+        }
+        
+    }
     
-    public String longestPalindrome(String s) {
+public String longestPalindrome(String s) {
         
-        if(s.length()<=1){
-            return s;
+        for(int i=0 ; i<s.length()-1 ; i++){
+            findStartEnd(s , i  , i);
         }
         
-        int n = s.length();
-        int start = 0;
-        int end = 0;
-        int max_len = 1;
-        
-        // for odd length string..
-        for(int i=0 ; i<n-1 ; i++){
-            int l = i;
-            int r = i;
-            
-            while(l>=0 && r<n){
-                if(s.charAt(l)==s.charAt(r)){
-                    l--;
-                    r++;
-                }
-                else break;
-            }
-            
-            int len = r-l-1;
-            if(len>max_len){
-                max_len = len;
-                start = l+1;
-                end = r-1;
-            }
+        for(int i=0 ; i<s.length()-1 ; i++){
+            findStartEnd(s , i , i+1);
         }
         
-        
-        
-        // for even length string...
-        for(int i=0 ; i<n-1 ; i++){
-            int l = i;
-            int r = i+1;
-            
-            while(l>=0 && r<n){
-                if(s.charAt(l)==s.charAt(r)){
-                    l--;
-                    r++;
-                }
-                else break;
-            }
-            
-            int len = r-l-1;
-            
-            if(len>max_len){
-                max_len = len;
-                start = l+1;
-                end = r-1;
-            }
-        }
-        
-        // returning answer....
-        return s.substring(start , end+1);
-        
+        return s.substring(temp[0] , temp[1]+1);
     }
 }
