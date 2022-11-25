def55019156a057b1266b99c5577f03ac7756c32
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-11-25 18:41:12 +0530

    Time: 30 ms (24.84%), Space: 41 MB (29.21%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,6 +3,7 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
+        
         int start = 1;
         int end = n;
         
@@ -13,13 +14,12 @@ public class Solution extends VersionControl {
             
             if(isBadVersion(mid)){
                 index = mid;
-                end = mid-1;
+                end  = mid-1;
             }
             else{
                 start = mid+1;
             }
         }
-        
         return index;
     }
 }
