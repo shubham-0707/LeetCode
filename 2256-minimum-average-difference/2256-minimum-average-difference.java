c37d0c57ec94c68f5f9221248d61bd1cef590560
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-12-04 08:29:16 +0530

    Time: 24 ms (70.09%), Space: 75.1 MB (57.86%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,6 +1,10 @@
 class Solution {
     public int minimumAverageDifference(int[] nums) {
         
+        // just calculate the prefix sum at all the indexes and then use it to calculate what is required in the question..
+        
+        // using long is a must if you are coding in java...
+        
         long sum = 0;
         for(int i=0 ; i<nums.length ; i++){
             sum+=nums[i];
@@ -23,6 +27,9 @@ class Solution {
                 minIdx=i;
             }
         }
+        
+        // Time Complexity : O(N)
+        // Space Complexity : O(1)
         return minIdx;
     }
 }
