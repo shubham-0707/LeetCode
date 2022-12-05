2c95529f455301a7b235129a6ea3ae5cca079523
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-12-05 21:48:52 +0530

    Time: 1 ms (38.48%), Space: 43.2 MB (23.36%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -15,12 +15,11 @@ class Solution {
         ListNode curr = head;
         
         while(curr!=null){
-            ListNode temp =  curr.next;
+            ListNode temp = curr.next;
             curr.next = prev;
             prev = curr;
             curr = temp;
         }
-        
         return prev;
     }
 }
