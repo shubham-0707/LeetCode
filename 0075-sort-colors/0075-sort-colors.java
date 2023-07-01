cd960e88efe3f4a19b3dba49d94648b253143faa
Shubham Singh <shubhamsinghlmp578@gmail.com>
2023-07-02 00:44:14 +0530

    Time: 0 ms (100%), Space: 41.1 MB (66.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,17 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for(int i=0 ; i<nums.length ; i++){
-            pq.add(nums[i]);
-        }
-        
-        for(int i=0 ; i<nums.length ; i++){
-            nums[i] = pq.poll();
-        }
-        
-        
+        int oneCount = 0;
+        int zeroCount = 0;
+        int twoCount = 0;
+       for(int i=0 ; i<nums.length ; i++){
+           if(nums[i]==0) zeroCount++;
+           else if(nums[i]==1) oneCount++;
+           else twoCount++;
+       }
+
+       for(int i=0 ; i<zeroCount; i++){
+           nums[i] = 0;
+       }
+
+       for(int i=zeroCount; i<(zeroCount+oneCount) ; i++){
+           nums[i] = 1;
+       }
+
+       for(int i=(zeroCount+oneCount) ; i<nums.length ; i++){
+           nums[i] = 2;
+       }
     }
 }
