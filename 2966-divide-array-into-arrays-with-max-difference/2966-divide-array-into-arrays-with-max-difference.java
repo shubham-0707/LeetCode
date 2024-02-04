9b9c6451e787a94c3ac14be2759dd6bf7f6d9ab4
Shubham Singh <shubhamsinghlmp578@gmail.com>
2024-02-04 23:29:23 +0530

    Time: 65 ms (13.66%), Space: 61.1 MB (27.63%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
@@ -6,21 +6,19 @@ class Solution {
         for(int i=0 ; i<nums.length ; i++){
             if(temp.size()==0){
                 temp.add(nums[i]);
-            } else {
-
+            } 
+            else {
                 if(Math.abs(nums[i]-temp.get(0))<=k){
-                    System.out.println("Add hua");
                 temp.add(nums[i]);
-            }
+                }
 
-            if(temp.size()==3){
+                if(temp.size()==3){
                 ans.add(temp);
-                System.out.println("Yahan pahucha");
                 temp = new ArrayList<>();
-            }
+                }
             }
         }
-        System.out.println("Answer size: "+ans.size());
+
         if(ans.size()*3!=nums.length) return new int[][]{};
         return ans.stream().map(innerList -> innerList.stream().mapToInt(Integer::intValue).toArray()).toArray(int[][]::new);
     }
