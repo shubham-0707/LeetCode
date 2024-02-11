feb780b931080ca18d99c1889bf51b5fe7849025
Shubham Singh <shubhamsinghlmp578@gmail.com>
2024-02-11 22:59:17 +0530

    Time: 425 ms (5.03%), Space: 46 MB (11.33%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,40 +1,44 @@
+class Frequency {
+    char character;
+    int frequency;
+
+    Frequency(char character , int frequency) {
+        this.character = character;
+        this.frequency = frequency;
+    }
+}
+
 class Solution {
     public String frequencySort(String s) {
         
-        // first we are going to use a hashmap to store the frequency of each element...
-        
-        HashMap<Character , Integer> hash = new HashMap<>();
-        for(int i=0 ; i<s.length() ; i++){
-            hash.put(s.charAt(i) , hash.getOrDefault(s.charAt(i) , 0)+1);
+        TreeMap<Character , Integer> tree = new TreeMap<Character , Integer>(Collections.reverseOrder());
+
+        for(int i=0 ; i<s.length() ; i++) {
+            tree.put(s.charAt(i), tree.getOrDefault(s.charAt(i), 0)+1);
         }
-        
-        // Now I am going to store the characters based on their frequency in an arraylist of character in a treemap sorted in descending order...
-        
-        TreeMap<Integer , ArrayList<Character>> tree = new TreeMap<>(Collections.reverseOrder());
-        for(char ch : hash.keySet()){
-            ArrayList<Character> temp = tree.get(hash.get(ch));
-            
-            if(temp==null){
-                temp = new ArrayList<>();
-                temp.add(ch);
-                tree.put(hash.get(ch) , temp);
-            }
-            else{
-                temp.add(ch);
-            }
+
+        List<Frequency> freqList = new ArrayList<>();
+        for(char ch : tree.keySet()) {
+            freqList.add(new Frequency(ch , tree.get(ch)));
         }
-        
-        // now getting the answer...
+
+        Collections.sort(freqList , new Comparator<Frequency>() {
+
+            public int compare(Frequency f0 , Frequency f1) {
+                return f1.frequency - f0.frequency;
+            }
+        } );
+
+
+
         String ans = "";
-        for(int i:tree.keySet()){
-            for(int j=0 ; j<tree.get(i).size() ; j++){
-                for(int k = 0; k<i ; k++){
-                    ans+=tree.get(i).get(j);
-                }
+
+        for(Frequency f : freqList) {
+            for(int i=0 ; i<f.frequency ; i++) {
+                ans+=f.character;
             }
         }
-        
+
         return ans;
-        
     }
 }
